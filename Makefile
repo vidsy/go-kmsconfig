40665438215a77a7c8816065d6c4f8a054d876cd
Steven Jack <stevenmajack@gmail.com>
2017-09-15 12:06:04 +0100

    Adds tests

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 BRANCH = "master"
+GO_BUILDER_IMAGE ?= "vidsyhq/go-builder"
+PATH_BASE ?= "/go/src/github.com/vidsy"
+REPONAME ?= "go-kmsconfig"
+TEST_PACKAGES ?= "./kmsconfig"
+
 VERSION = $(shell cat ./VERSION)
 
 check-version:
@@ -11,3 +16,13 @@ push-tag:
 	git pull origin ${BRANCH}
 	git tag ${VERSION}
 	git push origin ${BRANCH} --tags
+
+test:
+	@go test $(shell glide nv) -cover
+
+test-ci:
+	@docker run \
+	-v "${CURDIR}":${PATH_BASE}/${REPONAME} \
+	-w ${PATH_BASE}/${REPONAME} \
+	--entrypoint=go \
+	${GO_BUILDER_IMAGE} test "${TEST_PACKAGES}" -cover
---
+++ kmsconfig/config_test.go
@@ -0,0 +1,84 @@
+package kmsconfig_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vidsy/go-kmsconfig/kmsconfig"
+)
+
+func TestConfig(t *testing.T) {
+	configLocation := "./fixtures/config"
+
+	t.Run("LoadsConfigForDefaultEnvironment", func(t *testing.T) {
+		config := kmsconfig.NewConfig(configLocation)
+		err := config.Load()
+
+		assert.NoError(t, err)
+
+		stringValue, err := config.String("app", "test_string")
+		assert.NoError(t, err)
+		assert.Equal(t, "foo", stringValue)
+
+		intValue, err := config.Integer("app", "test_int")
+		assert.NoError(t, err)
+		assert.Equal(t, 1, intValue)
+
+		boolValue, err := config.Boolean("app", "test_bool")
+		assert.NoError(t, err)
+		assert.Equal(t, true, boolValue)
+	})
+
+	t.Run("LoadsConfigFromEnvironmentOverride", func(t *testing.T) {
+		err := os.Setenv("AWS_ENV", "test")
+		assert.NoError(t, err)
+
+		config := kmsconfig.NewConfig(configLocation)
+		err = config.Load()
+		assert.NoError(t, err)
+
+		stringValue, err := config.String("app", "test_string")
+		assert.NoError(t, err)
+		assert.Equal(t, "bar", stringValue)
+	})
+
+	t.Run("ErrorOnMissingEnvironment", func(t *testing.T) {
+		err := os.Setenv("AWS_ENV", "foo")
+		assert.NoError(t, err)
+
+		config := kmsconfig.NewConfig(configLocation)
+		err = config.Load()
+		os.Unsetenv("AWS_ENV")
+		assert.Error(t, err)
+	})
+
+	t.Run("NodeErrors", func(t *testing.T) {
+		config := kmsconfig.NewConfig(configLocation)
+		err := config.Load()
+		assert.NoError(t, err)
+
+		t.Run("MissingTopLevelNode", func(t *testing.T) {
+			_, err := config.String("foo", "bar")
+			assert.Error(t, err)
+		})
+
+		t.Run("MissingChildNode", func(t *testing.T) {
+			_, err := config.String("app", "foo")
+			assert.Error(t, err)
+		})
+	})
+
+	t.Run("OverrrideValue", func(t *testing.T) {
+		err := os.Setenv("VIDSY_VAR_app_test_string", "baz")
+		assert.NoError(t, err)
+
+		config := kmsconfig.NewConfig(configLocation)
+		err = config.Load()
+		os.Unsetenv("VIDSY_VAR_app_string_value")
+
+		stringValue, err := config.String("app", "test_string")
+		assert.NoError(t, err)
+		assert.Equal(t, "baz", stringValue)
+	})
+}
